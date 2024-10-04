b7788477f22d4c2170f9fb3b9a4a713d3f0039b7
teald <teal.dillon@gmail.com>
2024-10-03 17:35:17 -0700

    feat(cookiecutter):  Add initial cookiecutter.json, hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,9 @@
+"""Post-generation hooks for the intrument template.
+
+It does the following:
+
++ Installs a development version of DRAGONS into a virtual environment.
+"""
+
+def install_DRAGONS():
+    # TODO: Make this use the sessions in the now-generated noxfile.py

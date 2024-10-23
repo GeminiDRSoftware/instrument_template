2a112100585c18c0fe9ed05a47f96c07178ab1a6
teald <teal.dillon@gmail.com>
2024-10-23 14:21:13 -0700

    test: Add test of template.

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,13 @@
 """Noxfile meant for the cookiecutter repository itself."""
 
 from pathlib import Path
+import os
 import shutil
 
 import nox
 
 nox.options.sessions = ["cookiecutter_tests"]
+nox.options.error_on_external_run = True
 
 
 @nox.session(python="3.12")
@@ -55,3 +57,57 @@ def test(session: nox.Session):
     session.install("pytest", "pytest-cookies")
 
     session.run("pytest", *session.posargs)
+
+    session.notify("test_filled_template")
+
+
+@nox.session(python=["3.10", "3.11", "3.12"])
+def test_filled_template(session: nox.Session):
+    """Test a freshly generated template."""
+    session.install("cookiecutter", "pre-commit", "nox")
+
+    tmp_dir = Path(session.create_tmp()).resolve()
+
+    if tmp_dir.exists():
+        all_dirs = [root / dir for root, dirs, _ in os.walk(tmp_dir) for dir in dirs]
+        for path in all_dirs:
+            if path.exists():
+                shutil.rmtree(path)
+
+    template_dir = Path(".").resolve()
+
+    with session.chdir(".."):
+        session.run(
+            "cookiecutter",
+            "--no-input",
+            "--default-config",
+            str(template_dir),
+            "-o",
+            str(tmp_dir),
+        )
+
+    new_package_path = tmp_dir / "default_instrument_name_dr_package"
+
+    with session.chdir(tmp_dir):
+        assert new_package_path.exists()
+
+    with session.chdir(new_package_path):
+        # Create a development environment and ensure packages are installed.
+        session.run("nox", "-s", "devenv")
+
+        venv_loc = Path("venv")
+        venv_python = venv_loc / "bin" / "python"
+
+        test_imports = (
+            "import astrodata",
+            "import gemini_instruments",
+            "import geminidr",
+            "import gempy",
+        )
+
+        session.run(
+            str(venv_python),
+            "-c",
+            "\n".join(test_imports),
+            external=True,
+        )

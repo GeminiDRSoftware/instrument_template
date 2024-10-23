f737fc6348b7c1f3cc99cc00f5cbb0edcd65c2eb
teald <teal.dillon@gmail.com>
2024-10-23 12:39:46 -0700

    chore(template): Remove hooks dir.

--- hooks/post_gen_project.py
+++
@@ -1,9 +0,0 @@
-"""Post-generation hooks for the intrument template.
-
-It does the following:
-
-+ Installs a development version of DRAGONS into a virtual environment.
-"""
-
-def install_DRAGONS():
-    # TODO: Make this use the sessions in the now-generated noxfile.py

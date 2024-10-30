f4f69786d81db770de5a77667c676702f0e6b649
teald <teal.dillon@gmail.com>
2024-10-30 12:56:00 -0700

    feat(cookiecutter): Add post-gen git initialization

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,34 @@
+"""Post-generation hooks for the project."""
+
+import subprocess
+
+
+def git_init():
+    """Initialize a git repository here, and make the initial commit to it."""
+    init_command = ["git", "init"]
+    first_commit = [
+        "git",
+        "add",
+        ".",
+        "&&",
+        "git",
+        "commit",
+        "-m",
+        "Initial commit.",
+    ]
+
+    subprocess.run(init_command)
+    subprocess.run(first_commit)
+
+
+def main():
+    """This function runs after the repository is generated.
+
+    This means all names are filled in, all directories and files generated
+    and placed where they're expected to be.
+    """
+    git_init()
+
+
+if __name__ == "__main__":
+    main()
